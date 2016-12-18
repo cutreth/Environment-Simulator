4bcbccd087d576744d328e1130396c36608ec332
cutreth <kikot.world@gmail.com>
2016-12-18 01:29:21 -0600

    Spoofing RPi.GPIO for Windows

---
+++ GPIO.py
@@ -0,0 +1,17 @@
+
+BCM = "BCM"
+BOARD = "BOARD"
+OUT = "OUT"
+IN = "IN"
+LOW = "LOW"
+HIGH = "HIGH"
+
+def setmode(mode):
+    print("Using Pin Number mode " + mode)
+    return
+
+def setup(pin, mode):
+    print("Pin " + str(pin) + " configured as " + mode)
+    return
+
+
--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import platform
+isWindows = False
+if "Windows" in platform.platform():
+    isWindows = True
 
-print("Hello world!")
+if isWindows:
+    import GPIO as GPIO
+else:
+    import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(12, GPIO.IN)
+
+GPIO.output(18, GPIO.HIGH)
+GPIO.output(18, GPIO.LOW)
+
+if GPIO.input(12):
+    print("HIGH")
+else:
+    print("LOW")
 
