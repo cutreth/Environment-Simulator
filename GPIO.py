fc4d3ffb7e4a90933c066e337e9bdce991135abb
cutreth <kikot.world@gmail.com>
2016-12-18 14:15:38 -0600

    Spoofing RPi.GPIO for Windows

--- GPIO.py
+++ GPIO.py
@@ -5,13 +5,34 @@ OUT = "OUT"
 IN = "IN"
 LOW = "LOW"
 HIGH = "HIGH"
+PUD_UP = "UP"
+PUD_DOWN = "DOWN"
+
 
 def setmode(mode):
     print("Using Pin Number mode " + mode)
     return
 
-def setup(pin, mode):
-    print("Pin " + str(pin) + " configured as " + mode)
+
+def setup(pin, mode, pull_up_down = "NONE"):
+    print("Pin " + str(pin) + " configured as " + mode + " " + pull_up_down)
     return
 
 
+def output(pin, mode):
+    print("Pin " + str(pin) + " output of " + mode)
+    return
+
+
+def input(pin):
+    if pin % 2 == 0:
+        print("Pin " + str(pin) + " input of LOW")
+        output = False
+    else:
+        print("Pin " + str(pin) + " input of HIGH")
+        output = True
+    return output
+
+def cleanup():
+    print("Cleaning up")
+    return
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import platform
+import time
+
 isWindows = False
 if "Windows" in platform.platform():
     isWindows = True
@@ -12,13 +14,21 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(18, GPIO.OUT)
-GPIO.setup(12, GPIO.IN)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 GPIO.output(18, GPIO.HIGH)
 GPIO.output(18, GPIO.LOW)
 
-if GPIO.input(12):
+if GPIO.input(11):
     print("HIGH")
 else:
     print("LOW")
 
+if GPIO.input(14):
+    print("HIGH")
+else:
+    print("LOW")
+
+time.sleep(.25)
+GPIO.cleanup()
