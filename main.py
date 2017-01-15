1ba0b992e5abf98b961bcf4b9a990d036e4f71a7
cutreth <kikot.world@gmail.com>
2017-01-15 17:17:20 +0000

    General cleanup of logic; console logging added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+import datetime
 
 import do
 import light
 import temp
 
+print("Starting setup")
 
 config_light = {}
 config_light.update({'pin_out': 13})
@@ -19,24 +21,35 @@ config_humid = {}
 config_humid.update({'pin_out': 23})
 config_humid.update({'initial': GPIO.LOW})
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(config_light['pin_out'], GPIO.OUT, initial=config_light['initial'])
 GPIO.setup(config_temp['pin_out'], GPIO.OUT, initial=config_temp['initial'])
 GPIO.setup(config_humid['pin_out'], GPIO.OUT, initial=config_humid['initial'])
 
+state_temp = config_temp['initial']
+state_humid = config_humid['initial']
+
+print("Setup complete")
+
 try:
     while 1:
 
-        state_temp, state_humid = temp.check(config_temp['pin_in'])
-        state_temp = do.bool_to_out(state_temp)
-        GPIO.output(config_temp['pin_out'], state_temp)
+        print(datetime.datetime.now())
 
-        state_humid = do.bool_to_out(state_humid)
-        GPIO.output(config_humid['pin_out'], state_humid)
+        state_temp, state_humid = temp.check(config_temp['pin_in'], state_temp, state_humid)
+        out_temp = do.bool_to_out(state_temp)
+        GPIO.output(config_temp['pin_out'], out_temp)
+
+        out_humid = do.bool_to_out(state_humid)
+        GPIO.output(config_humid['pin_out'], out_humid)
 
         state_light = light.check()
-        state_light = do.bool_to_out(state_light)
-        GPIO.output(config_light['pin_out'], state_light)
+        out_light = do.bool_to_out(state_light)
+        GPIO.output(config_light['pin_out'], out_light)
+
+        print("Temp: " + str(state_temp))
+        print("Humid: " + str(state_humid))
+        print("Light: " + str(state_light))
 
         time.sleep(30)
 
--- temp.py
+++ temp.py
@@ -1,19 +1,23 @@
 import Adafruit_DHT
 
 
-def check(pin, cold=70, dry=40):
+def check(pin, temp, humid, temp_low=70, temp_high=75, humid_low=40, humid_high=60):
 
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, pin)
-    temperature * 9 / 5.0 + 32
+    temperature = round(temperature * 9 / 5.0 + 32, 1)
+    humidity = round(humidity, 1)
 
-    if temperature < cold:
+    print("Temp: " + str(temperature))
+    print("Humid: " + str(humidity))
+
+    if temperature < temp_low:
         temp = True
-    else:
+    elif temperature > temp_high:
         temp = False
 
-    if humidity < dry:
+    if humidity < humid_low:
         humid = True
-    else:
+    elif humidity > humid_high:
         humid = False
 
     return temp, humid
