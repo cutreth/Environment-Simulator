b267ec3d2e851ac6b2d811326d6250c672c1b12c
cutreth <kikot.world@gmail.com>
2017-01-15 19:03:47 -0600

    Continued Django config

--- rpi/envsim/admin.py
+++ rpi/envsim/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Reading
+from .models import Config
+
+
+class ReadingAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Values', {'fields': ['instant', 'temp_val', 'humid_val']}),
+        ('States', {'fields': ['temp_state', 'humid_state', 'light_state']}),
+    ]
+
+    readonly_fields = ('instant', )
+
+
+class ConfigAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('State', {'fields': ['temp_state', 'humid_state', 'light_state']})
+    ]
+
+    readonly_fields = ('instant', )
+
+admin.site.register(Reading, ReadingAdmin)
+admin.site.register(Config, ConfigAdmin)
--- rpi/envsim/models.py
+++ rpi/envsim/models.py
@@ -2,4 +2,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Reading(models.Model):
+
+    instant = models.DateTimeField('Instant', auto_now_add=True, unique=True)
+    temp_val = models.DecimalField('Temp Val', blank=True, null=True, max_digits=4, decimal_places=2)
+    humid_val = models.DecimalField('Humid Val', blank=True, null=True, max_digits=4, decimal_places=2)
+    temp_state = models.NullBooleanField('Temp State')
+    humid_state = models.NullBooleanField('Humid State')
+    light_state = models.NullBooleanField('Light State')
+
+
+class Config(models.Model):
+
+    instant = models.DateTimeField('Instant', auto_now=True)
+    temp_state = models.BooleanField('Temp State')
+    humid_state = models.BooleanField('Humid State')
+    light_state = models.BooleanField('Light State')
